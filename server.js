474b4236625f2c305bc3a08087c709713e9709f5
Emily Resch <eresch527@gmail.com>
2020-05-19 15:53:17 -0500

    rewrote server file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
+const mongoose =require("mongoose");
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
@@ -12,15 +12,14 @@ app.use(express.urlencoded({
 app.use(express.json());
 app.use(express.static("public"));
 
-//heroku connection w mongoose
+//heroku connection with mongoose
 const MONGODB_URL = process.env.MONGODB_URI || "mongodb://localhost/workouts";
 mongoose.connect(MONGODB_URL);
 
 app.use(apiRoutes);
 app.use(htmlRoutes);
 
-
-// Listen on port 3000
-app.listen(3000, () => { 
-    console.log("App running on port 3000!");
+//listening on port 3000
+app.listen(3000, () => {
+    console.log("App running on port 3000");
 });
